7be7b0fe7b64a2e683d0923ae9716fb289c1c05c
Nelson Lai <nelsonlai9503@gmail.com>
2022-11-22 09:26:27 +0000

    add aes encryption package

---
+++ aes/encrypt.go
@@ -0,0 +1,39 @@
+package aes
+
+import "crypto/aes"
+
+// Encrypt encrypts string to blob
+func Encrypt(raw string, key string) []byte {
+	src := []byte(raw)
+	cipher := getCipher(key)
+	length := (len(src) + aes.BlockSize) / aes.BlockSize
+	plain := make([]byte, length*aes.BlockSize)
+	copy(plain, src)
+	pad := byte(len(plain) - len(src))
+	for i := len(src); i < len(plain); i++ {
+		plain[i] = pad
+	}
+	encrypted := make([]byte, len(plain))
+
+	for bs, be := 0, cipher.BlockSize(); bs <= len(src); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
+		cipher.Encrypt(encrypted[bs:be], plain[bs:be])
+	}
+	return encrypted
+}
+
+// Decrypt decrypts blob to string
+func Decrypt(encrypted []byte, key string) string {
+	cipher := getCipher(key)
+
+	decrypted := make([]byte, len(encrypted))
+	for bs, be := 0, cipher.BlockSize(); bs < len(encrypted); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
+		cipher.Decrypt(decrypted[bs:be], encrypted[bs:be])
+	}
+
+	trim := 0
+	if len(decrypted) > 0 {
+		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
+	}
+
+	return string(decrypted[:trim])
+}
